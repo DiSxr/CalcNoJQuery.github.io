a1f3323132ac33ce5aa440a0cfb9d31860c87cd6
DiSxr <82447033+DiSxr@users.noreply.github.com>
2022-02-23 20:38:43 +0300

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,44 +1,60 @@
-$(document).ready(function() {
+$(document).ready(function()
+{
     var result = 0;
     var prevEntry = 0;
     var operation = null;
     var currentEntry = '0';
     updateScreen(result);
 
-    $("#clear").click(function(){
+    $("#clear").click(function()
+	{
         location.reload();
         console.log("to verify");
     });
 
-    $('.button').on('click', function(evt){
+    $('.button').on('click', function(evt)
+	{
         var buttonPressed = $(this).html();
         console.log(buttonPressed);
         
-        if(buttonPressed === "C"){
+        if(buttonPressed === "C")
+		{
             result = 0;
             currentEntry = '0';
-        } else if(buttonPressed === "CE") {
-            currentEntry = '0';
-        } else if(buttonPressed === "back") {
+        }
+		else if(buttonPressed === "←")
+		{
             currentEntry = currentEntry.substring(0, currentEntry.length-1);
-        } else if(buttonPressed === "+/-") {
-            currentEntry *= -1;
-        } else if(buttonPressed === '.') {
+        }
+		else if(buttonPressed === '.')
+		{
             currentEntry += '.';
-        } else if(isNumber(buttonPressed)) {
+        }
+		else if(isNumber(buttonPressed))
+		{
             if(currentEntry === '0') currentEntry = buttonPressed;
             else currentEntry = currentEntry + buttonPressed;
-        } else if(isOperator(buttonPressed)) {
+        }
+		else if(isOperator(buttonPressed))
+		{
             prevEntry = parseFloat(currentEntry);
             operation = buttonPressed;
             currentEntry = '';
-        } else if(buttonPressed === 'sqrt') {
+        }
+		else if(buttonPressed === '√')
+		{
             currentEntry = Math.sqrt(currentEntry);
-        } else if(buttonPressed === 'x^2') {
+        }
+		else if(buttonPressed === 'x²')
+		{
             currentEntry = currentEntry * currentEntry;
-        } else if(buttonPressed === '1/x') {
+        }
+		else if(buttonPressed === '1/x')
+		{
             currentEntry = 1 / currentEntry;
-        } else if(buttonPressed === '=') {
+        }
+		else if(buttonPressed === '=')
+		{
             currentEntry = operate(prevEntry, currentEntry, operation);
             operation = null;
         }
@@ -46,21 +62,25 @@ $(document).ready(function() {
     });
 });
 
-updateScreen = function(displayValue) {
+updateScreen = function(displayValue)
+{
     var displayValue = displayValue.toString();
     $('.screen').html(displayValue.substring(0,10));
     $('.text').html(displayValue.substring(0,10));
 };
 
-isNumber = function(value) {
+isNumber = function(value)
+{
     return !isNaN(value);
 }
 
-isOperator = function(value) {
+isOperator = function(value)
+{
     return value === '/' || value === '*' || value === '+' || value === '-' || value === '%';
 };
 
-operate = function(a, b, operation, c){
+operate = function(a, b, operation, c)
+{
     a = parseFloat(a);
     b = parseFloat(b);
     console.log(a, b, operation);
